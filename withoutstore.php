98acb63ac728a9b50cbcf5fcdca578fded91f60a
Bhuvnesh <37608973+bhuvisuthar@users.noreply.github.com>
2018-03-21 11:30:45 +0530

    Add files via upload
    Read csv file data into HTML table without storing the file

---
+++ withoutstore.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center><br><br><br><br>
+<table width="600">
+<form action="" method="post" enctype="multipart/form-data">
+
+<tr>
+<td width="20%">Select CSV file</td>
+<td width="80%"><input type="file" name="file" id="file" /></td>
+</tr>
+
+<tr></tr>
+<tr></tr>
+<tr>
+<td></td>
+<td><input type="submit" name="submit" /></td>
+</tr>
+
+</form>
+</table>
+</center>
+</body>
+</html>
+
+<?php
+if ( isset($_POST["submit"]) ) {
+
+   if ( isset($_FILES["file"])) {
+	   
+
+            //if there was an error uploading the file
+        if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+
+        }
+        else {
+              
+                 //if file already exists
+             if (file_exists("uploads/" . $_FILES["file"]["name"])) {
+             echo $_FILES["file"]["name"] . " already exists. ";
+             }
+             else {
+                    //Store file in directory "uploads"
+            
+              
+echo "<html><body><center><table border='2'>";
+$f = fopen($_FILES["file"]["tmp_name"], "r");
+while (($line = fgetcsv($f)) !== false) 
+{
+echo "<tr>";
+foreach ($line as $cell)
+        {
+echo "<td>" . htmlspecialchars($cell) . "</td>";
+        }
+echo "</tr>";
+}
+fclose($f);
+echo "</table></center></body></html>";
+
+            }
+        }
+     } else {
+             echo "No file selected <br />";
+     
+   }
+   
+}
+?>
+
